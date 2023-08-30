77b5b997bda7e05618f7df79c448fc6f5ed02a61
Carlos Rodríguez <juminstock@gmail.com>
2023-08-29 19:24:03 -0500

    Function get candidates in progress

--- contracts/dao/src/lib.rs
+++ contracts/dao/src/lib.rs
@@ -30,19 +30,41 @@ mod dao {
         votes: Mapping<AccountId, u64>,
         enabled_candidates: Mapping<AccountId, ()>
     }
+
     impl Dao {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { votante: init_value }
+        pub fn new(admin: AccountId) -> Self {
+            let now = Self::env().block_timestamp();
+            Self {
+                admin: Admin {
+                    address: admin,
+                    modified_date: now,
+                },
+                votes: Mapping::default(),
+                enabled_candidates: Mapping::default()
+            }
         }
 
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
         #[ink(message)]
-        pub fn get_votante(&mut self) -> bool {
-            self.votante
+        pub fn add_candidate(&mut self, candidate: AccountId) {
+            assert!(self.env().caller() == self.admin.address);
+
+            self.enabled_candidates.insert(candidate, &());
+            self.env().emit_event(NewCandidate { candidate });
         }
+
+        #[ink(message)]
+        pub fn vote(&mut self, candidate: AccountId) {
+            assert!(self.enabled_candidates.contains(candidate));
+
+            let candidate_votes = self.votes.get(candidate).unwrap_or(0);
+            self.votes.insert(candidate, &(candidate_votes + 1));
+            self.env().emit_event(Vote { candidate });
+        }
+
+        #[ink(message)]
+        pub fn get_candidates(&self) {
+        }
+
     }
 }
